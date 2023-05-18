ac4ca03cd7972c2a5c1f61f6f0e5edb1c76ced45
Andrew <80355579+Alekseevich-psk@users.noreply.github.com>
2023-05-18 10:34:01 +0300

    Initial commit

---
+++ js/swiper.js
@@ -0,0 +1,17 @@
+(function () {
+    const bannerSlider = document.querySelector(".banner__slider");
+    if (!bannerSlider) return;
+
+    new Swiper(".banner__slider", {
+        slidesPerView: 1,
+        spaceBetween: 6,
+        navigation: {
+            nextEl: ".banner__slider .sl-arrows__arrow--next",
+            prevEl: ".banner__slider .sl-arrows__arrow--prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        }
+    });
+})();
